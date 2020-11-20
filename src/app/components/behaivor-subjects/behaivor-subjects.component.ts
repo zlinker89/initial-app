85a4aa15148ef60f5a8a9d996f5a20b7f3d12a65
Franklin ospino <ospi89@hotmail.com>
2020-11-20 14:40:56 -0500

    subjects behaivor

--- src/app/components/behaivor-subjects/behaivor-subjects.component.ts
+++ src/app/components/behaivor-subjects/behaivor-subjects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-behaivor-subjects',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./behaivor-subjects.component.sass']
 })
 export class BehaivorSubjectsComponent implements OnInit {
-
-  constructor() { }
+  private numeroSubject: BehaviorSubject<number> = new BehaviorSubject(0);
+  numero = 0;
+  constructor() {
+    this.numeroSubject.subscribe(n => {
+      this.numero = n;
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  siguiente(): void{
+    const valor = Math.floor((Math.random() * 100) + 1);
+    console.log(this.numeroSubject.getValue());
+    this.numeroSubject.next(valor);
+  }
+
+  completar(): void{
+    this.numeroSubject.complete();
+  }
 }
